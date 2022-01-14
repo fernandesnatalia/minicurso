49cf24009741e4555dfc2eb0eb7825d32401f709
Natália Fernandes <88285066+fernandesnatalia@users.noreply.github.com>
2022-01-14 17:31:34 -0300

    Adicionar utilidade ao botao

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const botao = document.querySelector('button');
 const nome = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const status = document.querySelector('#status');
+
+
+capturarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers:{
+            Accept:'application/json',
+            "Content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+    });
+}
+
+botao.onclick = capturarPersonagem;
