12ce039cabd500be970b642e935250f39696b108
Natália Fernandes <88285066+fernandesnatalia@users.noreply.github.com>
2022-01-14 18:26:13 -0300

    Implementar as funcionalidades javascript

--- script.js
+++ script.js
@@ -2,19 +2,36 @@ const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nome = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
 
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Puxa, não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Mortinho da Silva';
+    } 
+}
+
+gerarValorAleatorio = () => {
+    return Math.floor(Math.random() * 671);
+}
 
 capturarPersonagem = () => {
-    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+    let numeroAleatorio = gerarValorAleatorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method:'GET',
-        headers:{
-            Accept:'application/json',
+        headers: {
+            Accept: 'application/json',
             "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
+        nome.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
     });
 }
 
